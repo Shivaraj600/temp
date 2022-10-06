0df9ec05905373b568e22994f5955d804117f1e8
Shivaraj600 <69134270+Shivaraj600@users.noreply.github.com>
2022-10-06 16:40:52 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,124 @@
+#VPC variables
+
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "my-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_azs" {
+  description = "Availability zones for VPC"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.0.0/20", "10.0.16.0/20"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.32.0/20", "10.0.48.0/20"]
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT gateway for VPC"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+#WEB_security group variables
+variable "web_sg_cidr" {
+  default = ["0.0.0.0/0"]
+}
+
+
+variable "web_sg_name" {
+  default = "web_SG"
+}
+#DB_security group variables
+variable "DB_sg_cidr" {
+  default = ["10.0.0.0/16"]
+}
+variable "db_sg_name" {
+  default = "DB_SG"
+}
+
+#EC2 variables
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "Web_server"
+}
+
+variable "ami" {
+  default = "ami-0c2b8ca1dad447f8a"
+}
+
+variable "key_name" {
+  default = "Terraform"
+}
+
+#RDS  variables
+variable "allocated_storage" {
+  default = "10"
+}
+
+variable "engine" {
+  default = "mysql"
+}
+
+variable "engine_version" {
+  default = "5.7.19"
+}
+
+variable "instance_class" {
+  default = "db.t2.micro"
+}
+
+variable "db_name" {
+  default = "mydb"
+}
+
+variable "user_name" {
+  default = "foo"
+}
+
+
+
+variable "publicly_accessible" {
+  default = "false"
+}
+
+variable "skip_final_snapshot" {
+  default = "true"
+}
+
+variable "major_engine_version" {
+  default = "5.7"
+}
+
+variable "family" {
+  default = "mysql5.7"
+}
